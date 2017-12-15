1a883488d3e8c9154e7bd77ab0f38b691309dc32
Asus Zen-Book\Montclair Robotics <montclairrobotics@montclairrobotics.org>
2017-12-15 15:09:13 -0500

    minor changes

--- src/main/java/frc/team555/robot/JoystickControls.java
+++ src/main/java/frc/team555/robot/JoystickControls.java
@@ -2,11 +2,24 @@ package frc.team555.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import org.montclairrobotics.sprocket.control.ArcadeDriveInput;
+import org.montclairrobotics.sprocket.control.Button;
 
 public class JoystickControls {
-    //Finals
-    public static final int DRIVE_STICK_PORT = 0;
 
-    Joystick driveStick    = new Joystick(DRIVE_STICK_PORT);
+    // Control Variables
+    public static final int DRIVE_STICK_PORT = 0;
+    public static final int AUX_STICK_PORT = 1;
+
+
+    // Drive Controls
+    Joystick driveStick = new Joystick(DRIVE_STICK_PORT);
     ArcadeDriveInput driveStickInput = new ArcadeDriveInput(driveStick);
+
+    // AUX Controls
+    Joystick auxStick = new Joystick(AUX_STICK_PORT);
+
+    public JoystickControls(){
+        //Buttons
+    }
+
 }
