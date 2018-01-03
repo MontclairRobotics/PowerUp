93a333bcb001120a27f329a1b514e90578a56e5d
Garrett Burroughs <garrettburroughs02@gmail.com>
2018-01-02 21:22:22 -0500

    Add comments to steamworks code

--- src/main/java/frc/team555/robot/Steamworks.java
+++ src/main/java/frc/team555/robot/Steamworks.java
@@ -7,10 +7,7 @@ import org.montclairrobotics.sprocket.SprocketRobot;
 import org.montclairrobotics.sprocket.control.Button;
 import org.montclairrobotics.sprocket.control.ButtonAction;
 import org.montclairrobotics.sprocket.control.JoystickButton;
-import org.montclairrobotics.sprocket.drive.DriveModule;
-import org.montclairrobotics.sprocket.drive.DriveTrainBuilder;
-import org.montclairrobotics.sprocket.drive.DriveTrainType;
-import org.montclairrobotics.sprocket.drive.InvalidDriveTrainException;
+import org.montclairrobotics.sprocket.drive.*;
 import org.montclairrobotics.sprocket.drive.steps.GyroCorrection;
 import org.montclairrobotics.sprocket.drive.utils.GyroLock;
 import org.montclairrobotics.sprocket.geometry.XY;
@@ -18,29 +15,49 @@ import org.montclairrobotics.sprocket.motors.Motor;
 import org.montclairrobotics.sprocket.utils.PID;
 
 public class Steamworks  extends SprocketRobot{
-
+    /*
+     * ========================================================================
+     *   Assign Device ID's
+     * ========================================================================
+     */
+    
+    // Drive train motors
     public final int frontLeftDeviceNumber = 3;
     public final int frontRightDeviceNumber = 1;
     public final int backLeftDeviceNumber = 4;
     public final int backRightDeviceNumber = 2;
-
+    
+    // Joysticks
     public final int driveStickDeviceNumber = 0;
     public final int auxStickDeviceNumber = 1;
-
+    
+    // Buttons
     public final int intakeButtonID = 1;
-
+    
+    
+    /*
+     * ========================================================================
+     *   Declare Hardware Devices
+     * ========================================================================
+     */
+    
+    // Intake Motors
     Motor intakeRight;
     Motor intakeLeft;
+    
+    // Intake
     DigitalInput openLeftSwitch;
     DigitalInput closeLeftSwitch;
     DigitalInput openRightSwitch;
     DigitalInput closeRightSwitch;
-
+    
+    // Drive Train Motors
     CANTalon drivetrainFL;
     CANTalon drivetrainFR;
     CANTalon drivetrainBL;
     CANTalon drivetrainBR;
-
+    
+    // Control Devices
     PowerDistributionPanel pdp;
     NavXRollInput navX;
 
@@ -50,49 +67,61 @@ public class Steamworks  extends SprocketRobot{
         Joystick auxStick = new Joystick(auxStickDeviceNumber);
         pdp = new PowerDistributionPanel();
 
-        //INTAKE
+        // ========= INTAKE ========= //
+        
+        // intake motors
         intakeRight = new Motor(new CANTalon(5));
         intakeRight.setInverted(true);
         intakeLeft = new Motor(new VictorSP(0));
         intakeLeft.setInverted(true);
+        
+        // intake limit switches
         openLeftSwitch = new DigitalInput(1);
         closeLeftSwitch = new DigitalInput(0);
         openRightSwitch = new DigitalInput(6);
         closeRightSwitch = new DigitalInput(7);
-
-        //DRIVETRAIN
+    
+        // ========= DRIVETRAIN ========= //
         drivetrainFL = new CANTalon(frontLeftDeviceNumber);
         drivetrainFR = new CANTalon(frontRightDeviceNumber);
         drivetrainBL = new CANTalon(backLeftDeviceNumber);
         drivetrainBR = new CANTalon(backRightDeviceNumber);
-
+        
+        // Gyro locking
         navX = new NavXRollInput(SPI.Port.kMXP);
         PID gyroPID = new PID(0.18*13.75,0,.0003*13.75);
         gyroPID.setInput(navX);
         GyroCorrection gCorrect=new GyroCorrection(navX,gyroPID,20,0.3*20);
         GyroLock gLock = new GyroLock(gCorrect);
-
+        
+        // Drive train setup
         DriveModule dtLeft  = new DriveModule(new XY(-1,0), new XY(0,-1), new Motor(drivetrainFL), new Motor(drivetrainBL));
         DriveModule dtRight = new DriveModule(new XY( 1,0), new XY(0, 1), new Motor(drivetrainFR), new Motor(drivetrainBR));
-
+        
         DriveTrainBuilder dtBuilder = new DriveTrainBuilder();
         dtBuilder.addDriveModule(dtLeft);
         dtBuilder.addDriveModule(dtRight);
+        
+        
+        //Drive train control
         dtBuilder.setDriveTrainType(DriveTrainType.TANK);
         dtBuilder.setArcadeDriveInput(driveStick);
         dtBuilder.addStep(gCorrect);
-
+        
+        // Create drive train
         try {
             dtBuilder.build();
         } catch (InvalidDriveTrainException e) {
             e.printStackTrace();
         }
-
-
-
-        // INTAKE CONTROL
+    
+    
+    
+        // ========= CONTROL ========= //
         double intakePow=0.3;
         Button intake = new JoystickButton(driveStick, intakeButtonID);
+        
+        // Intake Open
         intake.setHeldAction(new ButtonAction() {
             @Override
             public void onAction() {
@@ -100,7 +129,7 @@ public class Steamworks  extends SprocketRobot{
                 if(!openRightSwitch.get()){intakeRight.set(0);}else {intakeRight.set(intakePow);}
             }
         });
-
+        // Intake Close
         intake.setOffAction(new ButtonAction() {
             @Override
             public void onAction() {
