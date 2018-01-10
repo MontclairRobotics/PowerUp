a534483f9b356119b92bddf0976d8b31c0af25a8
Joshua Rapoport <j.wish.2012@gmail.com>
2018-01-10 16:43:23 -0500

    psv main was causing problems

--- src/main/java/frc/team555/robot/SideTest.java
+++ src/main/java/frc/team555/robot/SideTest.java
@@ -1,16 +1,16 @@
 package frc.team555.robot;
 
-public class SideTest {
-	public static void main(String[] args) {
-		String[] tests = {"L", "R", "LLL", "LLR", "LRL", "LRR", "RLL", "RLR", "RRL", "RRR"};
-		
-		for (int i = 0; i < tests.length; i++) {
-			System.out.print("(" + (i + 1) + ") " + tests[i] + ": ");
-			
-			for (Side s : Side.fromString(tests[i]))
-				System.out.print(s + " ");
-			
-			System.out.println();
-		}
-	}
-}
+//public class SideTest {
+//	public static void main(String[] args) {
+//		String[] tests = {"L", "R", "LLL", "LLR", "LRL", "LRR", "RLL", "RLR", "RRL", "RRR"};
+//		
+//		for (int i = 0; i < tests.length; i++) {
+//			System.out.print("(" + (i + 1) + ") " + tests[i] + ": ");
+//			
+//			for (Side s : Side.fromString(tests[i]))
+//				System.out.print(s + " ");
+//			
+//			System.out.println();
+//		}
+//	}
+//}
