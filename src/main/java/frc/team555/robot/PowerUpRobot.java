fe779dd35d60efe2957bcce32c50db8d6da41629
Will Chu <willcty@gmail.com>
2018-01-10 17:52:56 -0500

    added gyrolock

--- src/main/java/frc/team555/robot/PowerUpRobot.java
+++ src/main/java/frc/team555/robot/PowerUpRobot.java
@@ -1,29 +1,44 @@
 package frc.team555.robot;
 
+import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SPI;
 import org.montclairrobotics.sprocket.SprocketRobot;
 import org.montclairrobotics.sprocket.control.SquaredDriveInput;
 import org.montclairrobotics.sprocket.drive.DriveTrainBuilder;
 import org.montclairrobotics.sprocket.drive.DriveTrainType;
 import org.montclairrobotics.sprocket.drive.InvalidDriveTrainException;
+import org.montclairrobotics.sprocket.drive.steps.GyroCorrection;
+import org.montclairrobotics.sprocket.drive.utils.GyroLock;
+import org.montclairrobotics.sprocket.utils.DoubleInput;
+import org.montclairrobotics.sprocket.utils.PID;
 
 public class PowerUpRobot extends SprocketRobot{
 
     SprocketHardwareConfig hwConfig;
+    GyroLock gLock;
 
 
     //CONSTANTS
     public static final int driveStickPort = 0;
+    public static final int auxStickPort = 1;
 
+    public static final double pidP     = 0;
+    public static final double pidI     = 0;
+    public static final double pidD     = 0;
+    public static final double pidError = 0;
+    public static final double pidFarP  = 0;
 
     @Override
     public void robotInit() {
 
         // JOYSTICK
         Joystick driveStick = new Joystick(driveStickPort);
+        Joystick auxStick   = new Joystick(auxStickPort);
 
-        // DRIVETRAIN
+        // DRIVETRAIN & GYRO LOCK
         hwConfig = new SprocketHardwareConfig();
+
         DriveTrainBuilder dtBuilder = new DriveTrainBuilder();
         dtBuilder.addDriveModule(hwConfig.frontL);
         dtBuilder.addDriveModule(hwConfig.frontR);
@@ -31,6 +46,12 @@ public class PowerUpRobot extends SprocketRobot{
         dtBuilder.addDriveModule(hwConfig.backR);
         dtBuilder.setDriveTrainType(DriveTrainType.TANK);
         dtBuilder.setInput(new SquaredDriveInput(driveStick));
+
+        PID gyroPID = new PID(pidP, pidI, pidD);
+        gyroPID.setInput(hwConfig.navXYawInput);
+        GyroCorrection gCorrect=new GyroCorrection(hwConfig.navXYawInput, gyroPID,pidError,pidFarP);
+        gLock = new GyroLock(gCorrect);
+
         try {
             dtBuilder.build();
         } catch (InvalidDriveTrainException e) {
@@ -41,6 +62,6 @@ public class PowerUpRobot extends SprocketRobot{
 
     @Override
     public void update() {
-
+        gLock.update();
     }
 }
