5e073d66fa302ad505b02f65aa7d451da278c1b7
Joshua Rapoport <j.wish.2012@gmail.com>
2018-01-31 16:26:41 -0500

    Updated control scheme for Intake

--- src/main/java/frc/team555/robot/Control.java
+++ src/main/java/frc/team555/robot/Control.java
@@ -15,13 +15,17 @@ public class Control {
     // Drive Inputs
     public static DTInput driveInput;
     public static Button lock;
+    
+    // Intake Inputs
+    public static Button intakeL;
+    public static Button intakeR;
+    public static Button liftToggle;
 
     // Joystick Declaration
     public static Joystick driveStick;
     public static Joystick auxStick;
 
     public static void init() {
-
         // ==================
         // Joystick Creation
         // ==================
@@ -44,7 +48,28 @@ public class Control {
                 return driveStick.getRawButton(lockButtonID);
             }
         };
-
+        
+        // Intake Buttons
+        intakeL = new Button() {
+        		@Override
+            public Boolean get() {
+                return auxStick.getRawButton(4);
+            }
+        };
+        
+        intakeR = new Button() {
+    			@Override
+    			public Boolean get() {
+    				return auxStick.getRawButton(5);
+    			}
+        };
+        
+        liftToggle = new Button() {
+        		@Override
+        		public Boolean get() {
+        			return auxStick.getRawButton(7);
+        		}
+        };
 
         // Auxiliary Buttons
 
--- src/main/java/frc/team555/robot/CubeIntake.java
+++ src/main/java/frc/team555/robot/CubeIntake.java
@@ -5,21 +5,39 @@ import org.montclairrobotics.sprocket.loop.Updatable;
 import org.montclairrobotics.sprocket.loop.Updater;
 import org.montclairrobotics.sprocket.motors.Motor;
 import org.montclairrobotics.sprocket.utils.Input;
+import org.montclairrobotics.sprocket.utils.Togglable;
+import org.montclairrobotics.sprocket.utils.Utils;
 
-public class CubeIntake implements Updatable {
-	public final Motor left;
-	public final Motor right;
+public class CubeIntake implements Updatable, Togglable {
+	private static final double WEIGHT = 0.85;
+	private static final double WEIGHT_LR = 1.00 - WEIGHT;
 	
-	public final Input<Double> power;
+	private final Motor motorL;
+	private final Motor motorR;
+	
+	private final Input<Double> powerL;
+	private final Input<Double> powerR;
+	
 	
 	public CubeIntake() {
-		this.left = new Motor(Hardware.motorIntakeL);
-		this.right = new Motor(Hardware.motorIntakeR);
+		this.motorL = new Motor(Hardware.motorIntakeL);
+		this.motorR = new Motor(Hardware.motorIntakeR);
 		
-		this.power = new Input<Double>() {
+		this.powerL = new Input<Double>() {
 			@Override
 			public Double get() {
-				return -Control.auxStick.getY();
+				return Utils.constrain(
+						-Control.auxStick.getY()*WEIGHT - (Control.intakeL.get() ? WEIGHT_LR : 0) + (Control.intakeR.get() ? WEIGHT_LR : 0),
+						-1.0, +1.0);
+			}
+		};
+		
+		this.powerR = new Input<Double>() {
+			@Override
+			public Double get() {
+				return Utils.constrain(
+						-Control.auxStick.getY()*WEIGHT + (Control.intakeL.get() ? WEIGHT_LR : 0) - (Control.intakeR.get() ? WEIGHT_LR : 0),
+						-1.0, +1.0);
 			}
 		};
 		
@@ -28,7 +46,23 @@ public class CubeIntake implements Updatable {
 
 	@Override
 	public void update() {
-		left.set(power.get());
-		right.set(power.get());
+		if (Control.liftToggle.get()) { // Check if toggle button is pressed
+			disable();
+			// TODO: Use Hardware to enable Lift
+		}
+		
+		motorL.set(powerL.get());
+		motorR.set(powerR.get());
+	}
+
+	@Override
+	public void disable() {
+		motorL.set(0.0);
+		motorR.set(0.0);
+	}
+
+	@Override
+	public void enable() {
+		// TODO Auto-generated method stub
 	}
 }
