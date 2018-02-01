7ce6492acdd1002c0855d9b93cc68e914d361c50
Joshua Rapoport <j.wish.2012@gmail.com>
2018-02-01 15:06:13 -0500

    Removed weight system (used Utils.constrain() instead)

--- src/main/java/frc/team555/robot/CubeIntake.java
+++ src/main/java/frc/team555/robot/CubeIntake.java
@@ -8,9 +8,6 @@ import org.montclairrobotics.sprocket.utils.Input;
 import org.montclairrobotics.sprocket.utils.Utils;
 
 public class CubeIntake implements Updatable {
-	private static final double WEIGHT = 0.85;
-	private static final double WEIGHT_LR = 1.00 - WEIGHT;
-	
 	private final Motor motorL;
 	private final Motor motorR;
 	
@@ -25,14 +22,14 @@ public class CubeIntake implements Updatable {
 		this.powerL = new Input<Double>() {
 			@Override
 			public Double get() {
-				return -Control.auxStick.getY()*WEIGHT - (Control.auxIntakeL.get() ? WEIGHT_LR : 0) + (Control.auxIntakeR.get() ? WEIGHT_LR : 0);
+				return -Control.auxStick.getY() - (Control.auxIntakeL.get() ? 1.0 : 0.0) + (Control.auxIntakeR.get() ? 1.0 : 0.0);
 			}
 		};
 		
 		this.powerR = new Input<Double>() {
 			@Override
 			public Double get() {
-				return -Control.auxStick.getY()*WEIGHT + (Control.auxIntakeL.get() ? WEIGHT_LR : 0) - (Control.auxIntakeR.get() ? WEIGHT_LR : 0);
+				return -Control.auxStick.getY() + (Control.auxIntakeL.get() ? 1.0 : 0.0) - (Control.auxIntakeR.get() ? 1.0 : 0.0);
 			}
 		};
 		
