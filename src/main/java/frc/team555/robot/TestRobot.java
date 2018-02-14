684e83b641118c0a10f6c055fa05f56122ce6f04
Asus Zen-Book\Montclair Robotics <montclairrobotics@montclairrobotics.org>
2018-02-14 17:09:44 -0500

    Getting This Back

--- src/main/java/frc/team555/robot/TestRobot.java
+++ src/main/java/frc/team555/robot/TestRobot.java
@@ -17,10 +17,38 @@ public class TestRobot extends SprocketRobot{
     CubeIntake intake;
 
     public void robotInit() {
-        intake = new CubeIntake();
+        Hardware.init();
+        Control.init();
 
-        this.addAutoMode(new AutoMode("Test lift"));
-        this.sendAutoModes();
+
+
+        Input<Double> yAxis = new JoystickYAxis(0);
+        Input<Double> xAxisA = new JoystickXAxis(0);
+        Input<Double> xAxis= new Input<Double>(){
+
+            @Override
+            public Double get() {
+                if(Math.abs(xAxisA.get())<0.25)
+                {
+                    return 0.0;
+                }
+                return xAxisA.get();
+            }
+        };
+        Input<Double> leftInput = new Input<Double>() {
+            @Override
+            public Double get() {
+                return yAxis.get() - xAxis.get();
+            }
+        };
+        Input<Double> rightInput = new Input<Double>() {
+            @Override
+            public Double get() {
+                return yAxis.get() + xAxis.get();
+            }
+        };
+        ControlledMotor testMotor1 = new ControlledMotor(Hardware.testMotor1, leftInput);
+        ControlledMotor testMotor2 = new ControlledMotor(Hardware.testMotor2, rightInput);
 
     }
 }
